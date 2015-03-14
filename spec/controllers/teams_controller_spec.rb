a29adea71accb91d8d38d7a96bffeee00a07b31d
Vasiliy Ermolovich <younash@gmail.com>
2015-03-14 18:16:02 +0300

    Add spec for new action.

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -14,4 +14,14 @@ describe TeamsController do
       expect(response.body).to have_content('Team Jubilee')
     end
   end
+
+  describe '#new' do
+    it 'renders form' do
+      get :new
+
+      expect(response.body).to have_field('Name')
+      expect(response.body).to have_field('Description')
+      expect(response.body).to have_button('Save')
+    end
+  end
 end
