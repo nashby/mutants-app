783ed337e6abfd529d88874f34c3575e8579da89
Vasiliy Ermolovich <younash@gmail.com>
2015-03-14 23:43:53 +0300

    Add seed data.

---
+++ README.md
@@ -0,0 +1,4 @@
+### Setup
+
+* bundle install
+* rake db:setup
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+mutants = Mutant.create!([
+  {name: 'Angel'},
+  {name: 'Ariel'},
+  {name: 'Armor'},
+  {name: 'Aurora'},
+  {name: 'Beast'},
+  {name: 'Blink'},
+  {name: 'Box'}
+])
+
+tasks = Task.create!([
+  {name: 'Task 1'},
+  {name: 'Task 2'},
+  {name: 'Task 3'},
+  {name: 'Task 4'},
+  {name: 'Task 5'}
+])
+
+teams = Team.create!([
+  {name: 'Team 1', mutants: [mutants[0], mutants[1]]},
+  {name: 'Team 2', mutants: [mutants[2], mutants[3]]},
+  {name: 'Team 3', mutants: [mutants[4], mutants[5]]},
+  {name: 'Team 4', mutants: [mutants[0], mutants[3]]},
+  {name: 'Team 5', mutants: [mutants[4], mutants[2]]}
+])
+
