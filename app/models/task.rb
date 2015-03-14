5e05a3c5b9c71cdc26fe5172af0dedec8dc60724
Vasiliy Ermolovich <younash@gmail.com>
2015-03-14 18:53:37 +0300

    Add name presence validation for all models.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :team
+
+  validates :name, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ActiveRecord::Base
   has_and_belongs_to_many :mutants
+
+  validates :name, presence: true
 end
